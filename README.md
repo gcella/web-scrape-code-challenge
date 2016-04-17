2957afb77029ddf1637e6412b75da6840b77e312
Gabrielle Cella <gabriellecella@Gabrielles-MacBook-Pro.local>
2016-04-17 14:53:36 -0400

    create files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-scrape-code-challenge
+
+Gabrielle Cella
---
+++ find_email_addresses.py
@@ -0,0 +1,8 @@
+# Gabrielle Cella email address scraper coding challenge
+
+import sys
+import urllib2
+import re
+
+url = sys.argv[1]
+
